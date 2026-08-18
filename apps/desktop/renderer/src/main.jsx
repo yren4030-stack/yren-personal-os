2a69032c8f154ad6753f76269e8c15ca046c2f5d
yren4030-stack <yren4030@gmail.com>
2026-08-18 13:27:15 +0800

    feat: load macOS 26 shell refinements

--- apps/desktop/renderer/src/main.jsx
+++ apps/desktop/renderer/src/main.jsx
@@ -3,5 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './glass.css'
 import './macos26-liquid-glass.css'
+import './macos26-shell-refinement.css'
 
 createRoot(document.getElementById('root')).render(<App />)
